d68248a252709e5b821e42a3de7b2c9504b0a0b4
aaronchen <aaron.chen@onedegree.hk>
2021-07-02 23:54:17 +0800

    add test case

--- packages/utils/tests/date.test.ts
+++ packages/utils/tests/date.test.ts
@@ -1,4 +1,5 @@
 import { formatTimestamp } from "@shlack/utils";
+import { isTypedArray } from '@shlack/types';
 
 describe("formatTimestamp() tests", function () {
   let x = 4;
@@ -6,5 +7,32 @@ describe("formatTimestamp() tests", function () {
     expect(formatTimestamp(new Date("01-01-2020"))).toBe(
       "Jan 01, 2020 00:01:00 AM"
     );
+  });
+});
+
+
+
+describe("isTypedArray() tests", function () {
+  test("non-array", () => {
+    expect(
+      // @ts-expect-error
+      isTypedArray(null, () => true)
+    ).toEqual(false);
+  });
+  test("empty array", () => {
+    expect(isTypedArray([], (x: any): x is any => true)).toEqual(true);
+  });
+  test("homogenous array [1, 2, 3]", () => {
+    expect(
+      isTypedArray([1, 2, 3], (x): x is number => typeof x === "number")
+    ).toEqual(true);
+  });
+  test("mixed array [1, 'a', 3]", () => {
+    expect(
+      isTypedArray(
+        [1, "a", 3],
+        (x): x is number => ["number"].indexOf(typeof x) >= 0
+      )
+    ).toEqual(false);
   });
 });
